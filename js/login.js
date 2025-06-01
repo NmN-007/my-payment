117cd4a3ecbf51f606792b8fdf31f9e2130aeba3
NmN-007 <neazmahmudn@gmail.com>
2025-06-02 02:59:49 +0600

    first all fail add

---
+++ js/login.js
@@ -0,0 +1,14 @@
+//************************************************************************************** button *       
+document.getElementById('btn-login').addEventListener('click', function (event) {
+    event.preventDefault();
+    //********************************************************************************** your number * 
+    const yourNumber = document.getElementById('your-number').value;
+    //********************************************************************************** your Pin *
+    const yourPin = document.getElementById('your-Pin').value;
+    //********************************************************************************** phone and pin caking *
+    if (yourNumber === '7' && yourPin === '1234') {
+        window.location.href ='/home.html'}
+    else {
+        alert('wrong')
+    }
+})
