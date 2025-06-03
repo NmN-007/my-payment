0ffe71409a8012ec77bc93c0586c6d21395b1099
NmN-007 <neazmahmudn@gmail.com>
2025-06-03 22:26:26 +0600

    add balence compliet

--- js/home.js
+++ js/home.js
@@ -1,6 +1,15 @@
-document.getElementById('Add-Money-button').addEventListener('click', function(event){
+document.getElementById('Add-Money-button').addEventListener('click', function (event) {
     event.preventDefault();
     const inputAmount = document.getElementById('input-Amount').value;
     const inputPin = document.getElementById('input-Pin').value;
-    console.log(inputAmount, inputPin);
+    if (inputPin === '1234') {
+        const AvailableBalance = document.getElementById('Available-Balance').innerText;
+        const myManeBalance = parseFloat(AvailableBalance);
+        const inputAmountBalance = parseFloat(inputAmount);
+        const newBalance = myManeBalance + inputAmountBalance;
+        document.getElementById('Available-Balance').innerText = newBalance;
+    }
+    else {
+        alert('incaret info wrong');
+    }
 })
